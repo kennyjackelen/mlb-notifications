cf19238d8181c89e8c04ddd70faadcbfbd4c76cb
Kenny Jackelen <kennyjackelen@gmail.com>
2016-07-04 15:01:57 -0500

    debugging

--- app/src/lib/database.js
+++ app/src/lib/database.js
@@ -6,11 +6,13 @@ var MongoWrapper = function(){
 };
 
 MongoWrapper.prototype.init = function(){
+  console.log('database-init');
   this.mongoose = require('mongoose');
   this.connect();
 };
 
 MongoWrapper.prototype.connect = function(){
+  console.log('database-connect');
   this.mongoose.connect('mongodb://db/subscriptions');
   var db = this.mongoose.connection;
   db.on('error', console.error.bind( console, 'connection error:') );
@@ -18,11 +20,13 @@ MongoWrapper.prototype.connect = function(){
 };
 
 MongoWrapper.prototype._onConnectionOpened = function() {
+  console.log('database-_onConnectionOpened');
   var subscriptionSchema = this.mongoose.Schema( require('./subscription_schema.json') );
   this.Subscription = this.mongoose.model('Subscription', subscriptionSchema);
 };
 
 MongoWrapper.prototype.store = function( settings, subscriptionID, callback ) {
+  console.log('database-store');
   var record = new this.Subscription( { settings: settings, subscriptionID: subscriptionID } );
   record.save( callback );
 };
