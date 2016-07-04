5c13f19a64ac5b843e862771a89bd9b7ce387874
Kenny Jackelen <kennyjackelen@gmail.com>
2016-07-04 14:54:47 -0500

    add code to sync to server

--- app/src/index.js
+++ app/src/index.js
@@ -3,9 +3,11 @@
 
 var express = require('express');
 var path = require('path');
+var bodyParser = require("body-parser");
 var PORT = process.env.PORT || 8080;
 
 var app = express();
+app.use( bodyParser.json() );
 var router = express.Router();
 
 // set up routes
