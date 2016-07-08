8dd628ec55bc2540cf1122f2674dd2b61f44cfa8
Kenny Jackelen <kennyjackelen@gmail.com>
2016-07-07 22:01:27 -0500

    use request instead of http

--- app/src/lib/notify.js
+++ app/src/lib/notify.js
@@ -1,7 +1,7 @@
 /*jshint node:true,esnext:true*/
 'use strict';
 
-var http = require('http');
+var request = require('request');
 
 var queue = [];
 
@@ -15,52 +15,26 @@ module.exports = function( subscriptionID, currentPlay ) {
 };
 
 function notify( subscriptionID, currentPlay ) {
-  var headers = {
-    'Authorization' : 'key=' + process.env.GCM_API_KEY,
-    'Content-Type' : 'application/json'
-  };
-
-  console.log( 'notify' );
-
-  var options = {
-    host: 'android.googleapis.com',
-    port: 443,
-    path: '/gcm/send',
+  request( {
     method: 'POST',
-    headers: headers
-  };
-
-  var data = {
-    'delayWhileIdle': true,
-    'timeToLive': 3,
-    'data': {
-      'title': 'MLB Notification',
-      'message': 'Click me.'
+    url: 'https://android.googleapis.com/gcm/send',
+    headers: {
+      'Authorization' : 'key=' + process.env.GCM_API_KEY
     },
-    'registration_ids': [ subscriptionID ]
-  };
-  var dataString =  JSON.stringify(data);
-
-  var req = http.request(options, function(res) {
-    res.setEncoding('utf-8');
-    var responseString = '';
-
-    res.on('data', function(data) {
-      responseString += data;
-    });
-    res.on('end', function() {
-      // TODO: figure out how to clear the failed IDs.
-      console.log('response' + responseString);
-      if ( queue.length > 0 ) {
-        setTimeout( 100, notify.apply( this, ...queue.shift() ) );
-      }
-    });
-    console.log('STATUS: ' + res.statusCode);
-  });
-  req.on('error', function(e) {
-    console.log('error : ' + e.message + e.code);
-  });
-
-  req.write( dataString );
-  req.end();
+    json: true,
+    body: {
+      'delayWhileIdle': true,
+      'timeToLive': 3,
+      'data': {
+        'title': 'MLB Notification',
+        'message': 'Click me.'
+      },
+      'registration_ids': [ subscriptionID ]
+    }
+  },
+  function() {
+    if ( queue.length > 0 ) {
+      setTimeout( 100, notify.apply( this, ...queue.shift() ) );
+    }
+  } );
 }
