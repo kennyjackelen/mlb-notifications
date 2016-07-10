b314d94b8afade31674f4815ef402898995c1102
Kenny Jackelen <kennyjackelen@gmail.com>
2016-07-10 13:25:11 -0500

    debug against CIN

--- app/src/lib/gameday.js
+++ app/src/lib/gameday.js
@@ -29,7 +29,7 @@ function getTwinsGame() {
           }
           for ( var i = 0; i < schedule.length; i++ ) {
             var game = schedule[ i ];
-            if ( game.away_name_abbrev === 'MIN' || game.home_name_abbrev === 'MIN' ) {
+            if ( game.away_name_abbrev === 'CIN' || game.home_name_abbrev === 'CIN' ) {
               twinsGame.date = date;
               twinsGame.game_data_directory = game.game_data_directory;
               twinsGame.game = game;
